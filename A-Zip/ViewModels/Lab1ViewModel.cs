582cc9490ae58d6d26eaef8a4f3a72ed35978436
Никита Фомин <defdXs@yandex.ru>
2024-10-03 16:54:54 +0300

    Added some UI elements

--- A-Zip/ViewModels/Lab1ViewModel.cs
+++ A-Zip/ViewModels/Lab1ViewModel.cs
@@ -1,9 +1,12 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Windows.Storage;
 
 namespace A_Zip.ViewModels;
 
 public partial class Lab1ViewModel : ObservableRecipient
 {
+    public StorageFile SelectedFile;
+
     public Lab1ViewModel()
     {
     }
